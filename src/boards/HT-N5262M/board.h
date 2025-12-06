16f47ad5579a00c1f11bbacb1b414e4098dbfe53
Winston Lowe <wel97459@gmail.com>
2025-12-06 09:45:13 -0800

    added HT-N5262M

---
+++ src/boards/HT-N5262M/board.h
@@ -0,0 +1,78 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2018 Ha Thach for Adafruit Industries
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+#ifndef _HELTEC_T114_H
+#define _HELTEC_T114_H
+
+#define _PINNUM(port, pin)    ((port)*32 + (pin))
+
+/*------------------------------------------------------------------*/
+/* LED
+ *------------------------------------------------------------------*/
+#define LEDS_NUMBER           1
+#define LED_PRIMARY_PIN       _PINNUM(0, 14) // Green
+#define LED_STATE_ON          0
+
+#define LED_NEOPIXEL          _PINNUM(0, 15)
+#define NEOPIXEL_POWER_PIN    _PINNUM(0, 21)
+#define NEOPIXELS_NUMBER      2
+#define BOARD_RGB_BRIGHTNESS  0x040404
+
+/*------------------------------------------------------------------*/
+/* BUTTON
+ *------------------------------------------------------------------*/
+#define BUTTONS_NUMBER        2
+#define BUTTON_1              _PINNUM(1, 10)
+#define BUTTON_2              _PINNUM(1, 10)
+#define BUTTON_PULL           NRF_GPIO_PIN_PULLUP
+
+//--------------------------------------------------------------------+
+// BLE OTA
+//--------------------------------------------------------------------+
+#define BLEDIS_MANUFACTURER   "Heltec AutoMation"
+#define BLEDIS_MODEL          "HT-n5262"
+
+//--------------------------------------------------------------------+
+// USB
+//--------------------------------------------------------------------+
+#define USB_DESC_VID           0x239A
+#define USB_DESC_UF2_PID       0x0071
+#define USB_DESC_CDC_ONLY_PID  0x0071
+
+
+//--------------------------------------------------------------------+
+// WDT
+//--------------------------------------------------------------------+
+
+#define WDT_ENABLED       1
+#define WAKE_PIN   _PINNUM(0, 13)   // P0.13
+#define DONE_PIN   _PINNUM(0, 11)   // P0.11
+
+//------------- UF2 -------------//
+#define UF2_PRODUCT_NAME      "HT-n5262"
+#define UF2_VOLUME_LABEL      "HT-n5262"
+#define UF2_BOARD_ID          "HT-n5262"
+#define UF2_INDEX_URL         "HT-n5262"
+
+#endif
